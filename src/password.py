b8af42a9c9628f4599219a07f0bdee5ba8f07016
Melon1566 <asthomas1@liberty.edu>
2022-08-19 13:50:28 -0400

    Implemented string decryption

--- src/password.py
+++ src/password.py
@@ -1,10 +1,24 @@
 import helpers
-
+from Crypto.PublicKey import RSA
+from Crypto.Cipher import PKCS1_v1_5 as Cipher_PKCS1_v1_5
+import base64
 
 def resetPassword(data):
     try:
-        print(data)
-        arguments = (data["NewPassword"], data["Email"], data["OldPassword"], data["OldPassword"])
+        
+        email = data["Email"]
+
+        with open('private.pem', 'r') as f:
+            private_key = f.read()
+            
+        private = RSA.importKey(private_key)
+        cipher2 = Cipher_PKCS1_v1_5.new(private)
+
+        byteString = cipher2.decrypt(base64.b64decode(email), "error")
+        decryptedString = byteString.decode()
+
+
+        arguments = (data["NewPassword"], decryptedString, data["OldPassword"], data["OldPassword"])
         helpers.masterCursor.execute("update Users set Password = SHA1(%s) where email = %s and (Password = SHA1(%s) or Password = %s)", arguments)
 
         arguments = (data["Email"], data["NewPassword"])
